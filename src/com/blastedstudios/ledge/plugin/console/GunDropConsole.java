7ab82081d59c7a02f62db58ad7e7c06718cee5bd
narfman0 <narfman0@gmail.com>
2014-05-04 14:53:43 -0400

    -Allow to drop a specific weapon

--- src/com/blastedstudios/ledge/plugin/console/GunDropConsole.java
+++ src/com/blastedstudios/ledge/plugin/console/GunDropConsole.java
@@ -7,6 +7,7 @@ import com.blastedstudios.gdxworld.util.Properties;
 import com.blastedstudios.ledge.util.IConsoleCommand;
 import com.blastedstudios.ledge.world.DropManager;
 import com.blastedstudios.ledge.world.WorldManager;
+import com.blastedstudios.ledge.world.weapon.WeaponFactory;
 
 @PluginImplementation
 public class GunDropConsole implements IConsoleCommand{
@@ -20,9 +21,16 @@ public class GunDropConsole implements IConsoleCommand{
 				Gdx.app.log("GunDropConsole.execute", "Gun drop probability: " + 
 						Properties.getFloat(DropManager.GUN_DROP_PROPERTY, .05f));
 			else if(tokens.length == 3){
-				Properties.set(DropManager.GUN_DROP_PROPERTY, "" + Float.parseFloat(tokens[2]));
-				Gdx.app.log("GunDropConsole.execute", "Gun drop probability set to: " + 
-						Properties.getFloat(DropManager.GUN_DROP_PROPERTY));
+				try{
+					Properties.set(DropManager.GUN_DROP_PROPERTY, "" + Float.parseFloat(tokens[2]));
+					Gdx.app.log("GunDropConsole.execute", "Gun drop probability set to: " + 
+							Properties.getFloat(DropManager.GUN_DROP_PROPERTY));
+				}catch(NumberFormatException e){
+					String weapon = tokens[2];
+					world.getPlayer().getGuns().add(0, WeaponFactory.getWeapon(weapon));
+					world.getPlayer().setCurrentWeapon(0, world.getWorld());
+					Gdx.app.log("GunDropConsole.execute", "Generated weapon: " + weapon + " for player");
+				}
 			}
 		}
 	}
