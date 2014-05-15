6e2cda890315e5d7bb5387a90f9dc59b588a1609
narfman0 <narfman0@gmail.com>
2014-05-15 11:03:43 -0400

    -Use logarithmic formula to figure out volume given distance

--- src/com/blastedstudios/ledge/plugin/gunlistener/GunshotSound.java
+++ src/com/blastedstudios/ledge/plugin/gunlistener/GunshotSound.java
@@ -18,7 +18,7 @@ public class GunshotSound implements IGunListener {
 	@Override public void shoot(Gun gun, Being source, Random random, Vector2 direction, WorldManager world, Vector2 origin) {
 		final Sound sound = SoundManager.getSound(gun.getFireSound());
 		if(sound != null){
-			float volume = 1f - Math.min(1, world.getPlayer().getPosition().dst(origin) / 25f);
+			float volume =  (float)Math.min(1, 1.0/Math.log(world.getPlayer().getPosition().dst(origin)+1f));
 			float pan = Math.max(-1, Math.min(1, (world.getPlayer().getPosition().x - origin.x) / 15f));
 			sound.play(volume, 1, pan);
 		}else
